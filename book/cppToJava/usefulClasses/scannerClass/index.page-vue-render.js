b0bcc866314abf7479868ce2f7e75a0f27f173b0
Damith C. Rajapakse <damith@gmail.com>
2022-01-24 22:32:40 +0800

    Site Update. [skip ci]

--- book/cppToJava/usefulClasses/scannerClass/index.page-vue-render.js
+++ book/cppToJava/usefulClasses/scannerClass/index.page-vue-render.js
@@ -23,6 +23,6 @@ with(this){return _c('pre',[_c('code',{pre:true,attrs:{"class":"hljs java"}},[_c
 with(this){return _c('p',[_c('code',{pre:true,attrs:{"class":"hljs inline no-lang"}},[_v("Scanner")]),_v(" class normally reads inputs as strings but it can read in a specific type of input too.")])}
 },function anonymous(
 ) {
-with(this){return _c('footer',[_c('div',{staticClass:"text-center"},[_c('small',[_v("["),_c('span',[_c('strong',[_v("Powered by")])]),_v(" "),_c('img',{attrs:{"src":"https://markbind.org/favicon.ico","width":"30"}}),_v(" "),_c('a',{attrs:{"href":"https://markbind.org/"}},[_v("MarkBind 3.1.0")]),_v(", generated on Sat, 22 Jan 2022, 22:18:50 GMT+8]")])])])}
+with(this){return _c('footer',[_c('div',{staticClass:"text-center"},[_c('small',[_v("["),_c('span',[_c('strong',[_v("Powered by")])]),_v(" "),_c('img',{attrs:{"src":"https://markbind.org/favicon.ico","width":"30"}}),_v(" "),_c('a',{attrs:{"href":"https://markbind.org/"}},[_v("MarkBind 3.1.1")]),_v(", generated on Mon, 24 Jan 2022, 22:29:37 GMT+8]")])])])}
 }];
   
